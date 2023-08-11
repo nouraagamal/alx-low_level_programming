0715788c09dbd3080f92393ae44c105281f9465c
nouraagamal <nouragamal011@gmail.com>
2023-08-11 06:25:52 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
- /**
+
+/**
   *main - Entry point
   *
   *Description: print a quote using write function.
@@ -7,9 +8,9 @@
   * Return: 1 (not success)
 */
 
+
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";	
 	write(1, quo, 59);
 	return (1);
 }
