06d52661bedeeceff5447c5593bcc5d98ab570a8
nouraagamal <nouragamal011@gmail.com>
2023-08-11 06:19:25 -0700

    ااع

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,7 @@
  /**
   *main - Entry point
   *
-  *Description: print a quote using write function
-  *          write(int fd.const void *buf.size_t count);
+  *Description: print a quote using write function.
   *
   * Return: 1 (not success)
 */
@@ -11,7 +10,6 @@
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
 	write(1, quo, 59);
 	return (1);
 }
