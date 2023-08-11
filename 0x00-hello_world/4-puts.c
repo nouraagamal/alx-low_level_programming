0dc69e9eba7782f63c4b782beb468a8894c8f164
nouraagamal <nouragamal011@gmail.com>
2023-08-11 03:47:24 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * prints put fn
+ * Description: A C program that prints with put function
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
