14ed9ea9c2289663bf8f06737be675389baa3e79
nouraagamal <nouragamal011@gmail.com>
2023-08-11 13:53:23 -0700

    c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: printing all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int d1 = 0, d2, d3;
+
+	while (d1 <= 9)
+	{
+		d2 = 0;
+		while (d2 <= 9)
+		{
+			d3 = 0;
+			while (d3 <= 9)
+			{
+				if (d1 != d2 && d1 < d2 && d2 != d3 && d2 < d3)
+				{
+					putchar(d1 + 48);
+					putchar(d2 + 48);
+					putchar(d3 + 48);
+
+					if (d1 + d2 + d3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				d3++;
+			}
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+	return (0);
+}
