6d95e357285b15c5cfe3239379d49843bf82d4a8
nouraagamal <nouragamal011@gmail.com>
2023-08-15 14:54:06 -0700

    bom

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *
+ *Description: a program that prints _putchar
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
