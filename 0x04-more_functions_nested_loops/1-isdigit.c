f7ce5c17aba51927104462d53130750fbb011e01
nouraagamal <nouragamal011@gmail.com>
2023-08-17 16:02:48 -0700

    b

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - check if c is upper
+ * _isdigit - check if bet 0 - 9
  *
- * @c: input for alphabet
+ * @c: input
  *
- * Return: 1 if yppercase. 0 if not
+ * Return: 1 if digit. 0 if not
 */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
