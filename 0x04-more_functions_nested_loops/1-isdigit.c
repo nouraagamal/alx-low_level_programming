c9bb0dc3102730f90bf680473c36ae921de0d7e7
nouraagamal <nouragamal011@gmail.com>
2023-08-17 16:00:32 -0700

    b

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if yppercase. 0 if not
+*/
+
+int _isupper(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
