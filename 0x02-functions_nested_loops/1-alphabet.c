59d51dbe3d56894dbaba9465a42eab46cf318d5a
nouraagamal <nouragamal011@gmail.com>
2023-08-22 01:09:01 -0700

    f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ * return: success (0)
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 0; i <= 26; i++)
+	{
+		_putchar(97 + i);
+	}
+	_putchar('\n');
+}
