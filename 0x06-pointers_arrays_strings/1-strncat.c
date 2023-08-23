ffd4bc90c0cca1dc245d122168f6028651740b65
nouraagamal <nouragamal011@gmail.com>
2023-08-23 07:21:52 -0700

    d

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,9 +14,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	if (src <= n)
-	{
+	int l = 0;
+	int i;
 
+	while (dest[l])
+		l++;
 
-	printf("%p", *dest + *src);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[l + i] = src[i];
+	dest[l + i] = '\0';
+	return ('dest');
 }
