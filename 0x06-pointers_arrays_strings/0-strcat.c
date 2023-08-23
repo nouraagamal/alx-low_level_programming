dd67c2a55578d0f199e3b00fdeec1a28aabb681f
nouraagamal <nouragamal011@gmail.com>
2023-08-23 05:36:19 -0700

    d

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *_strcat - a function that concatenates two strings
+ *
+ * @dest: input char
+ * @src: input char
+ *
+ * Return: returns a pointer to 'dest'
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	printf("%p", *dest + *src);
+	return ('dest');
+}
