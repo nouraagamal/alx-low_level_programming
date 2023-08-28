4ecde1cad89a748d91835866a74f0da49b2ff9bf
nouraagamal <nouragamal011@gmail.com>
2023-08-28 15:49:54 -0700

    e

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,12 +12,13 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 	char *p;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
+		j = 0;
 		while (accept[j] != '\0')
 		{
 			if (accept[j] == s[i])
