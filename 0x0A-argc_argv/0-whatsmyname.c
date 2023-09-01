97c054e3f2f7c1104207070db0e037739d09154f
nouraagamal <nouragamal011@gmail.com>
2023-09-01 16:33:50 -0700

    h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name,
+ *
+ * @argc: int
+ * @argv: int
+ *
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
