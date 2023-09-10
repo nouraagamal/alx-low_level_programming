7fc89b38062bab989a5ce1a3e6a51961870d99b1
nouraagamal <nouragamal011@gmail.com>
2023-09-10 04:14:26 -0700

    k

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *c;
-	int i = 0, j = 0, size1 = 0, size2 = 0;
+	unsigned int i = 0, j = 0, size1 = 0, size2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
