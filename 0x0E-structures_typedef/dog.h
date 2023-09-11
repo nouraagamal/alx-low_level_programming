eb5115a9799e24e5f4ff3f33813e9ab05c5019a0
nouraagamal <nouragamal011@gmail.com>
2023-09-11 14:40:20 -0700

    l

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a struct
+ *
+ * @name: the dog's name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: nothing
+*/
+
 struct dog 
 {
 	char * name;
