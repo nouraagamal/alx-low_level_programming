6c31268478628969d69fc74fa01f48eb06f910e0
nouraagamal <nouragamal011@gmail.com>
2023-09-11 18:29:12 -0700

    l

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - makes a new struct dog
+ *
+ * @name: his/her name
+ * @age: how old is he
+ * @owner: who are you
+ *
+ * Return: new dog if fails null
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int size = 0,i = 0;
+
+	while(*str)
+	{
+		size++;
+		*str++;
+	}
+
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees dogs.
+ *
+ * @d: pointer to dog to be freed.
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
