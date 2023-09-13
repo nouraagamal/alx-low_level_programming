523958cf4fe94e9f83729f4a06af656659c382fc
nouraagamal <nouragamal011@gmail.com>
2023-09-13 15:19:04 -0700

    ;

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+* get_op_func - Selects correct function to perform based on s
+*
+* @s: The operator
+*
+* Return: The pointer to the operator's function
+* NULL if not found
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+/**
+* main - Entry point
+*
+* @argc: length of command line arguments
+*
+* @argv: pointer
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int (*fn)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argc[1]);
+	b = atoi(argv[3]);
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	fn = get_op_func(argv[2]);
+	if (fn == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n",fn(a, b));
+	return (0);
+}
