12b16921a076a212f070f61a5bf2b76c19606d0a
nouraagamal <nouragamal011@gmail.com>
2023-09-13 15:40:14 -0700

    ;

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,18 +2,61 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+* op_add - Adds two numbers
+*
+* @a: A number1
+*
+* @b: A number2
+*
+* Return: Sum
+*/
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+* op_sub - Calculates the difference of 2 numbers
+*
+* @a: A number1
+*
+* @b: A number2
+*
+* Return: Difference
+*/
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+* op_mul - Multiplies two numbers
+*
+* @a: A number1
+*
+* @b: A number2
+*
+* Return: Product
+*/
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
+/**
+* op_div - Divides @a by @b
+*
+* @a: A number1
+*
+* @b: A number2
+*
+* Return: Result of the division
+*/
+
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -23,6 +66,17 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+
+/**
+* op_mod - Calculates the remainder of a divided by b
+*
+* @a: A number1
+*
+* @b: A number2
+*
+* Return: Remainder
+*/
+
 int op_mod(int a, int b)
 {
 	if (b == 0)
