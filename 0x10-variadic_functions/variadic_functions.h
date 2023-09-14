e1a91d647b0ebf29830e7fc9544967c4034c4724
nouraagamal <nouragamal011@gmail.com>
2023-09-14 16:32:12 -0700

    o

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC_FUNCTIONS_H_
 #define VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
+/**
+* struct format_struct - format-type class
+* @format: format identifier
+* @fmt_print_func: pointer to function that prints in the format of @format
+*/
+
+struct format_struct
+{
+	char format;
+	void (*fmt_print_func)(va_list *arg_list);
+};
+typedef struct format_struct format_struct_ptr;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
