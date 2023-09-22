b35862e00ab1bb980696eed57e2e9e213f553f74
nouraagamal <nouragamal011@gmail.com>
2023-09-21 17:34:57 -0700

    r

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,14 +3,14 @@
 /**
  * list_len - print elements of a list
  *
- * @h:  number of elements in a linked list
+ * @h: number of elements in a linked list
  *
  * Return: number of nodes
 */
 
 size_t list_len(const list_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
 	while (h)
 	{
