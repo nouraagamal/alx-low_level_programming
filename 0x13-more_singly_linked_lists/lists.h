13d24506f746014a29fa2c370e99d84f0bf17a1e
nouraagamal <nouragamal011@gmail.com>
2023-09-26 14:57:21 -0700

    p

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -7,16 +7,18 @@
 
 /**
  * struct listint_s - singly linked list
+ *
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  *
  */
+
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
