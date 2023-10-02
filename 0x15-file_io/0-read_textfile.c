77e5527cdf53ecf18bc48595fd134cd3773a67fe
nouraagamal <nouragamal011@gmail.com>
2023-10-02 16:01:51 -0700

    k

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fild < 0)
+	if (fd < 0)
 		return (0);
 	byte = read(fd, buffer, letters);
 	byte = write(STDOUT_FILENO, buffer, byte);
