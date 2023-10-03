0c3a5ec76b8a35fcc079fbc8c158059af627fd4f
nouraagamal <nouragamal011@gmail.com>
2023-10-03 03:37:56 -0700

    o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,25 +3,25 @@
 /**
  * read_textfile - Reads a textfile and prints the contents to the POSIX STDOUT
  *
- * @filename: The name of the file to read from
- * @letters: The number of characters it should print to the STDOUT
+ * @filename: The name of the file
+ * @letters: The number of characters it should print
  *
  * Return: Returns the number of characters printed
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, byte;
+	ssize_t openfile, bytes = 0;
 	char *buffer;
 
+	if (!filename)
+		return (0);
+	openfile = open(filename, O_RDONLY);
+	if (openfile == -1)
+		return (0);
 	buffer = malloc(sizeof(char) * (letters));
-	if (!filename || !letters)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-	byte = read(fd, buffer, letters);
-	byte = write(STDOUT_FILENO, buffer, byte);
-	close(fd);
-	return (byte);
+	bytes = read(openfile, buffer, letters);
+	bytes = write(STDOUT_FILENO, buffer, bytes);
+	close(openfile);
+	return (bytes);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int -putchar(char c);
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
