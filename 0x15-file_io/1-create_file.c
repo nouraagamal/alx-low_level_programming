71f8489d60073a1b4d31bde6e0a3ade449876d79
nouraagamal <nouragamal011@gmail.com>
2023-10-03 04:13:55 -0700

    o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (!text_content)
-		text_content = "";
+		text_content = " ";
 	openfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (openfile == -1)
 		return (-1);
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - This function adds text at end of file
+ *
+ * @filename: name of the file to append data to.
+ * @text_content: data to append to file
+ *
+ * Return: 1 if succesfull else -1 on err
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t openfile, bytes, len;
+
+	if (!filename)
+		return (-1);
+	for (len = 0; text_content; len++)
+		;
+	if (text_content)
+	{
+		bytes = write(openfile, text_content, len);
+		if (bytes == -1)
+			return (-1);
+	}
+	close(openfile);
+	return (1);
+}
