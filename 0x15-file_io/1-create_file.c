1e9c6cf1a42230a532fd2779d502fa19118df7af
nouraagamal <nouragamal011@gmail.com>
2023-10-03 03:51:00 -0700

    dn

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and adds permission to it
+ *
+ * @filename: The name of the file to be created
+ * @text_content: content to be placed in the created file
+ *
+ * Return: 1 if successfull and -1 on err.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t openfile = 0; bytes = 0, len = 0;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	openfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (openfile == -1)
+		return (-1);
+	for (len = 0; text_content; len ++)
+		;
+	bytes = write(openfile, text_content, len);
+	close(openfile);
+	return (bytes == len ? 1 : -1);
+}
