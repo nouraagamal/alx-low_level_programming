322599c404e16f2dd8f276cf96facb792dde0415
nouraagamal <nouragamal011@gmail.com>
2023-10-03 03:52:57 -0700

    dn

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t openfile = 0; bytes = 0, len = 0;
+	ssize_t openfile, bytes, len;
 
 	if (!filename)
 		return (-1);
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	openfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (openfile == -1)
 		return (-1);
-	for (len = 0; text_content; len ++)
+	for (len = 0; text_content; len++)
 		;
 	bytes = write(openfile, text_content, len);
 	close(openfile);
