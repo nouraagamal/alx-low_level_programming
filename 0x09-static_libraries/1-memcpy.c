cef45c15b3c6cddaa9b895948d3999023fc73878
nouraagamal <nouragamal011@gmail.com>
2023-10-28 12:31:21 -0700

    memcpy function

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * _memcpy - a function to copy array to another one
+ * of bigger size
+*/
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{
 		dest[i] = src[i];
+	}
 	return (dest);
 }
