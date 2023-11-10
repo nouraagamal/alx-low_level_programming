9661beb4c14c69a433714abf6d105e92b83d4ef8
nouraagamal <nouragamal011@gmail.com>
2023-11-10 12:15:14 -0800

    .

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,16 +11,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
-	if (!head)
+	if (!head || !new_node)
 		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
 	new_node->n = n;
-	new_node->next = (*head);
 	new_node->prev = NULL;
 	if (*head != NULL)
+	{
+		new_node->next = (*head);
 		(*head)->prev = new_node;
-	(*head) = new_node;
+		(*head) = new_node;
+	}
+	else
+	{
+		(*head) = new_node;
+		new_node->next = NULL;
+	}
 	return (new_node);
 }
