d181a6624bcbe8b65a5852e61f0a7a1e001aab6b
nouraagamal <nouragamal011@gmail.com>
2023-11-10 12:43:03 -0800

    .

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,15 +2,13 @@
 
 /**
  * free_dlistint - list
- * @head: list
- *
- * Return: list
+ * @head: pointer to first node
+ * Return: nothing
  */
 
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *x = head;
-
 	while (x)
 	{
 		dlistint_t *next = x->next;
