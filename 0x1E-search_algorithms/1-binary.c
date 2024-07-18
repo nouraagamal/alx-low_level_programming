eeb017cea35a4614183c492f22f14f7616780dc7
nouraagamal <nouragamal011@gmail.com>
2024-07-17 19:56:50 -0700

    .

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -20,7 +20,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		for (start = 0, end = size - 1; end >= start;)
 		{
-			printf("Searching in array:");
+			printf("Searching in array: ");
 			for (i = start; i < end; i++)
 				printf("%d, ", array[i]);
 			printf("%d\n", array[i]);
