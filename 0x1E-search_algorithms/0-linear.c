d7f17944649daa2fb87fea444cd256bd090fe977
nouraagamal <nouragamal011@gmail.com>
2024-07-17 18:16:06 -0700

    ..

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,6 +21,7 @@ int linear_search(int *array, size_t size, int value)
 	}
 	else {
 		for (i = 0; i < size; i++) {
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			if (array[i] == value) {
 				return (i);
 			}
